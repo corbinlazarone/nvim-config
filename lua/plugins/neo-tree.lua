5f1f611809c1f3b4bacf60233bd25d67ca09e1c8
Corbin Lazarone <corbinlazarone1@gmail.com>
2026-06-10 13:17:04 -0500

    neo tree takes up full window

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,9 +1,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
   opts = {
     window = {
-      position = "float",
+      position = "current",
     },
   },
 }
